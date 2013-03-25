b72dc3f3fc0d2a6dc46bf6ccb583da803f2613fc
bstuder <trosk@laposte.net>
2013-03-25 15:52:45 +0100

    fix scan suggestions

--- Model/Sample.hs
+++ Model/Sample.hs
@@ -8,19 +8,19 @@ import Model.Types
 import Model.Profile
 
 newtype Sample = Sample [Joule]
-	deriving (Eq, Show)
+  deriving (Eq, Show)
 
 instance ToJSON Sample where
-	toJSON (Sample xs) = toJSON xs
+  toJSON (Sample xs) = toJSON xs
 
 instance FromJSON Sample where
-	parseJSON = fmap Sample . parseJSON
+  parseJSON = fmap Sample . parseJSON
 
 computeSample :: Second -> Profile -> Sample
 computeSample t p = Sample $ go p
   where
-  	go :: Profile -> [Joule]
-  	go p' = x : go (n' `deferredBy` (-t))
-  	  where
-  	  	(n, n') = split p' t
-  	  	x       = computeEnergy 0 t n
+    go :: Profile -> [Joule]
+    go p' = x : go (n' `deferredBy` (-t))
+      where
+        (n, n') = split p' t
+        x       = computeEnergy 0 t n
