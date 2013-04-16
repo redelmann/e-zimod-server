cca2740ce3ae10f7622b19b8cc10376726ea087a
Romain Edelmann <romain.edelmann@gmail.com>
2013-04-16 11:10:19 +0200

    Added the sampling time to Sample.

--- Model/Sample.hs
+++ Model/Sample.hs
@@ -1,22 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Model.Sample where
+
+import Control.Applicative ((<*>), (<$>))
+import Control.Monad (mzero)
 
 import Data.Aeson
 
 import Model.Types
 import Model.Profile
 
-newtype Sample = Sample [Joule]
+data Sample = Sample Second [Joule]
                deriving (Eq, Show)
 
 instance ToJSON Sample where
-    toJSON (Sample xs) = toJSON xs
+    toJSON (Sample t xs) = object ["time" .= t, "data" .= xs]
 
 instance FromJSON Sample where
-    parseJSON = fmap Sample . parseJSON
+    parseJSON (Object v) = Sample <$> v .: "time" <*> v .: "data"
+    parseJSON _ = mzero
 
 computeSample :: Second -> Profile -> Sample
-computeSample t p = Sample $ go 0 p
+computeSample t p = Sample t $ go 0 p
   where
     go :: Rational -> Profile -> [Joule]
     go i p' = x : go (i + 1) n'
