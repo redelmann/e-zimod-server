b6e907a3a7aa99eb128c1a518bdeacc9aab69e86
Romain Edelmann <romain.edelmann@gmail.com>
2013-05-09 09:43:27 +0200

    Added KiloWattHour as a unit of energy.

--- Model/Types.hs
+++ Model/Types.hs
@@ -4,11 +4,27 @@ module Model.Types (
       Watt
     , Second
     , Joule
+    , KiloWattHour
+    , jouleToKiloWattHour
+    , kiloWattHourToJoule
     ) where
+
+-- | Unit of time.
+type Second = Rational
 
 -- | Unit of power.
 type Watt = Rational
+
 -- | Unit of energy.
 type Joule = Rational
--- | Unit of time.
-type Second = Rational
+
+-- | Unit of energy.
+type KiloWattHour = Rational
+
+-- | Converts from Joules to Kilo Watts per Hour.
+jouleToKiloWattHour :: Joule -> KiloWattHour
+jouleToKiloWattHour = (/ 3600000)
+
+-- | Converts from Kilo Watts per Hour to Joules.
+kiloWattHourToJoule :: KiloWattHour -> Joule
+kiloWattHourToJoule = (* 3600000)
